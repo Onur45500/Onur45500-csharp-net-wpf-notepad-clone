c7a382f71eb6a3bfcc37e74da62b2662f7c29ed2
Onur45500 <onur.akmese@viacesi.fr>
2024-10-15 13:53:08 +0200

    Initialized the project

---
+++ csharp-net-wpf-notepad-clone/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csharp_net_wpf_notepad_clone
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

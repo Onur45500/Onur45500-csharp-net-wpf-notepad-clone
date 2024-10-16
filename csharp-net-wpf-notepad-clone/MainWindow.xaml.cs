b4eba3d8b2dce34e93ffbb627b833ad181873222
Onur45500 <onur.akmese@viacesi.fr>
2024-10-16 16:17:20 +0200

    Implemented the open function

--- csharp-net-wpf-notepad-clone/MainWindow.xaml.cs
+++ csharp-net-wpf-notepad-clone/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,62 @@ namespace csharp_net_wpf_notepad_clone
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool match = true;
+
+        string filePath = "";
+        string titleName = "";
+        string documentContent = "";
+        
+        private string DocumentContent
+        {
+            get => documentContent;
+            set => documentContent = value;
+        }
+
+        private string TitleName
+        {
+            get => titleName;
+            set => titleName = value;
+        }
+
+        private string FilePath
+        {
+            get => filePath;
+            set => filePath = value;
+        }
+
+        private bool Match
+        {
+            get => match;
+            set => match = value;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Open_FileDialog()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+
+            if(ofd.FileName != null)
+            {
+                FilePath = ofd.FileName;
+                TitleName = ofd.SafeFileName;
+                Match = true;
+
+                StreamReader FileReader = new StreamReader(FilePath);
+                DocumentContent = FileReader.ReadToEnd();
+                TextArea.Text = DocumentContent;
+                FileReader.Close();
+            }
+        }
+
+        private void Open_MenuItem(object sender, RoutedEventArgs e)
+        {
+            Open_FileDialog();
         }
     }
 }
